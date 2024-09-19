5890da6c2ac538b5cc14b839eeaa5445f5c7d77a
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2024-09-19 06:03:23 +0530

    Initialized FireBase

---
+++ lib/homepage.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Notes Application"),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+
+    );
+  }
+}
